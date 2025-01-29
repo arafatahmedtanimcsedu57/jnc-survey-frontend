84b9b65516663a7440b6bd3022102810c9f821d7
NaziaJahanMeem <nazia.jahan@g.bracu.ac.bd>
2025-01-29 19:18:28 +0600

    rekove login

--- src/components/FormTemplates/NewFormDialogComponent.tsx
+++ src/components/FormTemplates/NewFormDialogComponent.tsx
@@ -1,9 +1,9 @@
 import React, {
-	FormEventHandler,
-	FunctionComponent,
-	PropsWithChildren,
-	useEffect,
-	useState,
+  FormEventHandler,
+  FunctionComponent,
+  PropsWithChildren,
+  useEffect,
+  useState,
 } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Dialog, DialogContent, TextField } from '@mui/material';
@@ -17,189 +17,189 @@ import useModalStrip from '../../global-hooks/useModalStrip';
 import { TemplateType } from '../../types/FormTemplateTypes';
 
 interface NewFormDialogComponentProps {
-	openDialog: boolean;
-	setOpenDialog: (arg: boolean) => void;
+  openDialog: boolean;
+  setOpenDialog: (arg: boolean) => void;
 }
 
 const NewFormDialogComponent: FunctionComponent<
-	PropsWithChildren<NewFormDialogComponentProps>
+  PropsWithChildren<NewFormDialogComponentProps>
 > = (props) => {
-	const [creatingForm, setCreatingForm] = useState<boolean>(false);
+  const [creatingForm, setCreatingForm] = useState<boolean>(false);
 
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-	const { showModalStrip } = useModalStrip();
-	const authToken = useAppSelector((state) => state.user.access.token);
-	const {
-		loading: loginLoading,
-		error: loginError,
-		success: loginSuccess,
-	} = useAppSelector((state) => state.user.auth);
+  const { showModalStrip } = useModalStrip();
+  const authToken = useAppSelector((state) => state.user.access.token);
+  const {
+    loading: loginLoading,
+    error: loginError,
+    success: loginSuccess,
+  } = useAppSelector((state) => state.user.auth);
 
-	const handleLogin: FormEventHandler<HTMLFormElement> = async (e) => {
-		e.preventDefault();
+  const handleLogin: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
 
-		const username = e.currentTarget.username.value || '';
-		const password = e.currentTarget.password.value || '';
+    const username = e.currentTarget.username.value || '';
+    const password = e.currentTarget.password.value || '';
 
-		const data = {
-			username,
-			password,
-		};
+    const data = {
+      username,
+      password,
+    };
 
-		try {
-			const { data: _data } = await dispatch(getLogin(data)).unwrap();
-			dispatch(setToken(_data));
-		} catch {}
-	};
+    try {
+      const { data: _data } = await dispatch(getLogin(data)).unwrap();
+      dispatch(setToken(_data));
+    } catch {}
+  };
 
-	const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-		e.preventDefault();
-		const newFormData = {
-			formName: e.currentTarget.formName.value,
-			formId: e.currentTarget.formId.value,
-		};
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const newFormData = {
+      formName: e.currentTarget.formName.value,
+      formId: e.currentTarget.formId.value,
+    };
 
-		setCreatingForm(true);
+    setCreatingForm(true);
 
-		try {
-			const template: TemplateType = await dispatch(
-				addTemplate(newFormData),
-			).unwrap();
-			navigate(`/formbuilder/${template.publishStatus}-${template.formId}`);
-		} catch (ex) {
-			showModalStrip(
-				'danger',
-				'Error occurred while creating a new Form',
-				5000,
-			);
-		}
-	};
+    try {
+      const template: TemplateType = await dispatch(
+        addTemplate(newFormData)
+      ).unwrap();
+      navigate(`/formbuilder/${template.publishStatus}-${template.formId}`);
+    } catch (ex) {
+      showModalStrip(
+        'danger',
+        'Error occurred while creating a new Form',
+        5000
+      );
+    }
+  };
 
-	useEffect(() => {
-		if (!authToken) dispatch(getToken('GET AUTH TOKEN'));
-	}, []);
+  // useEffect(() => {
+  // 	if (!authToken) dispatch(getToken('GET AUTH TOKEN'));
+  // }, []);
 
-	return (
-		<>
-			<Dialog
-				open={props.openDialog}
-				fullWidth
-				maxWidth="sm"
-				onClose={() => {
-					props.setOpenDialog(false);
-				}}
-			>
-				{authToken ? (
-					<DialogContent className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title">Enter the following details</h5>
-						</div>
+  return (
+    <>
+      <Dialog
+        open={props.openDialog}
+        fullWidth
+        maxWidth="sm"
+        onClose={() => {
+          props.setOpenDialog(false);
+        }}
+      >
+        {/* {authToken ? ( */}
+        <DialogContent className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Enter the following details</h5>
+          </div>
 
-						<div className="modal-body py-4">
-							<form onSubmit={handleFormSubmit}>
-								<div className="mb-3">
-									<TextField
-										required
-										size="small"
-										label="Form Name"
-										name="formName"
-										id="formName"
-										key="formName"
-										className="form-control"
-										defaultValue=""
-									/>
-								</div>
+          <div className="modal-body py-4">
+            <form onSubmit={handleFormSubmit}>
+              <div className="mb-3">
+                <TextField
+                  required
+                  size="small"
+                  label="Form Name"
+                  name="formName"
+                  id="formName"
+                  key="formName"
+                  className="form-control"
+                  defaultValue=""
+                />
+              </div>
 
-								<div className="mb-3">
-									<TextField
-										required
-										size="small"
-										label="Form ID"
-										name="formId"
-										id="formId"
-										key="formId"
-										className="form-control"
-										defaultValue=""
-									/>
-								</div>
+              <div className="mb-3">
+                <TextField
+                  required
+                  size="small"
+                  label="Form ID"
+                  name="formId"
+                  id="formId"
+                  key="formId"
+                  className="form-control"
+                  defaultValue=""
+                />
+              </div>
 
-								<button
-									type="submit"
-									className="btn btn-sm btn-success fw-medium px-4"
-									disabled={creatingForm}
-								>
-									{creatingForm ? (
-										<>
-											<span
-												className="spinner-border spinner-border-sm mr-2"
-												role="status"
-												aria-hidden="true"
-											></span>
-											Loading
-										</>
-									) : (
-										'Create Form'
-									)}
-								</button>
-							</form>
-						</div>
-					</DialogContent>
-				) : (
-					<DialogContent className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title">Login</h5>
-						</div>
+              <button
+                type="submit"
+                className="btn btn-sm btn-success fw-medium px-4"
+                disabled={creatingForm}
+              >
+                {creatingForm ? (
+                  <>
+                    <span
+                      className="spinner-border spinner-border-sm mr-2"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                    Loading
+                  </>
+                ) : (
+                  'Create Form'
+                )}
+              </button>
+            </form>
+          </div>
+        </DialogContent>
+        {/* ) : (
+          <DialogContent className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Login</h5>
+            </div>
 
-						<div className="modal-body py-4">
-							<form onSubmit={handleLogin}>
-								<div className="mb-3">
-									<TextField
-										size="small"
-										label="User Name"
-										name="username"
-										className="form-control"
-										id="userName"
-										key="userName"
-									/>
-								</div>
+            <div className="modal-body py-4">
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <TextField
+                    size="small"
+                    label="User Name"
+                    name="username"
+                    className="form-control"
+                    id="userName"
+                    key="userName"
+                  />
+                </div>
 
-								<div className="mb-3">
-									<TextField
-										size="small"
-										label="Password"
-										type="password"
-										name="password"
-										id="password"
-										key="password"
-										className="form-control"
-									/>
-								</div>
-								<button
-									type="submit"
-									className="btn btn-sm btn-primary px-4 fw-medium"
-								>
-									{loginLoading ? (
-										<>
-											<span
-												className="spinner-border spinner-border-sm mr-2"
-												role="status"
-												aria-hidden="true"
-											></span>
-											Loading
-										</>
-									) : (
-										'Log In'
-									)}
-								</button>
-							</form>
-						</div>
-					</DialogContent>
-				)}
-			</Dialog>
-		</>
-	);
+                <div className="mb-3">
+                  <TextField
+                    size="small"
+                    label="Password"
+                    type="password"
+                    name="password"
+                    id="password"
+                    key="password"
+                    className="form-control"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-sm btn-primary px-4 fw-medium"
+                >
+                  {loginLoading ? (
+                    <>
+                      <span
+                        className="spinner-border spinner-border-sm mr-2"
+                        role="status"
+                        aria-hidden="true"
+                      ></span>
+                      Loading
+                    </>
+                  ) : (
+                    'Log In'
+                  )}
+                </button>
+              </form>
+            </div>
+          </DialogContent>
+        )} */}
+      </Dialog>
+    </>
+  );
 };
 
 export default NewFormDialogComponent;
--- src/pages/TemplatesPage.tsx
+++ src/pages/TemplatesPage.tsx
@@ -14,15 +14,12 @@ const TemplatesPage: React.FC<PropsWithChildren<TemplatesPageProps>> = ({}) => {
     (state) => state.entities.formBuilder.allTemplates
   );
 
-  const authToken = useAppSelector((state) => state.user.access.token);
-
   const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   useEffect(() => {
-    if (authToken) {
-      dispatch(getAllTemplates('GET ALL TEMPLATE'));
-    }
-  }, [authToken]);
+    dispatch(getAllTemplates('GET ALL TEMPLATE'));
+  }, []);
+  console.log(templates);
 
   return (
     <div className="container-fluid">
@@ -38,42 +35,36 @@ const TemplatesPage: React.FC<PropsWithChildren<TemplatesPageProps>> = ({}) => {
             </div>
           </div>
           <div className="d-flex flex-wrap gap-5">
-            {/* Create New */}
             <FormLayoutComponent
               template={null}
               createdFormLayout={false}
               setOpenDialog={setOpenDialog}
-            />
-
-            {authToken ? (
-              <>
-                <div className="w-100 text-center">
-                  <h6 className="text-primary">Single or multi-page forms</h6>
-                  <h3 className="text-info-emphasis fw-bolder">
-                    Discover our beautiful templates
-                  </h3>
-                  <div className="text-muted">
-                    Our collection of beautiful templates to create your own
-                    forms!
-                  </div>
+            />{' '}
+            <>
+              <div className="w-100 text-center">
+                <h6 className="text-primary">Single or multi-page forms</h6>
+                <h3 className="text-info-emphasis fw-bolder">
+                  Discover our beautiful templates
+                </h3>
+                <div className="text-muted">
+                  Our collection of beautiful templates to create your own
+                  forms!
                 </div>
-                <div className="d-flex flex-wrap gap-5 justify-content-center w-100">
-                  {templates.length ? (
-                    templates.map((template) => (
-                      <FormLayoutComponent
-                        key={template.id}
-                        template={template}
-                        createdFormLayout={true}
-                      />
-                    ))
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              </>
-            ) : (
-              <></>
-            )}
+              </div>
+              <div className="d-flex flex-wrap gap-5 justify-content-center w-100">
+                {templates.length ? (
+                  templates.map((template) => (
+                    <FormLayoutComponent
+                      key={template.id}
+                      template={template}
+                      createdFormLayout={true}
+                    />
+                  ))
+                ) : (
+                  <></>
+                )}
+              </div>
+            </>
             <hr />
             {/* confirm bhai */}
             {/* {authToken ? (
